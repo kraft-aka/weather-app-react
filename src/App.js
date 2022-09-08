51d98682d11c1a4e6a4f6725c79f6d870ca6a947
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-08 17:05:59 +0200

    set up the project, init app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [city, setCity] = useState("");
+
+  const API_KEY = "fb804751dcaaf6b0ad41f3ea9c65892e";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  const getCity = (e) => {
+    if (e.key === "Enter") {
+      axios.get(url).then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      });
+    }
+  };
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-search">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyPress={getCity}
+          placeholder="Enter your city"
+        />
+        {city && (
+          <button className="app-btn" onClick={refreshPage}>
+            Refresh
+          </button>
+        )}
+      </div>
+
+      <div className="app-container">
+        <div className="top-section">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temperature">
+            {data.main ? <h1>{data.main.temp.toFixed()} C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p>{data.main.feels_like.toFixed()} C</p> : null}
+            </div>
+            <div className="wind">
+              {data.wind ? <p>{data.wind.speed} MPH</p> : null}
+            </div>
+            <div className="humidity">
+              {data.main ? <p>{data.main.humidity} %</p> : null}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
