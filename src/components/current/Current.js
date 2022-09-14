4f306d9e490db390986222af9a3d3004b732f064
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-14 10:27:48 +0200

    style current

--- src/components/current/Current.js
+++ src/components/current/Current.js
@@ -1,8 +1,8 @@
 //import { useNavigate } from "react-router-dom";
+import "./Current.css";
 // provides current weather data
 
 const Current = (props) => {
-
   //const navigate = useNavigate();
   return (
     <div className="current">
@@ -13,32 +13,34 @@ const Current = (props) => {
               {props.data.name} {props.data.sys.country}
             </h3>
           </div>
-          <div className="current-temp">
-            <h1>{props.data.main.temp.toFixed()}°C </h1>
-            <img
-              src={`http://openweathermap.org/img/w/${props.data.weather[0].icon}.png`}
-            />
-          </div>
-          <div className="current-description">
-            <p>{props.data.weather[0].main}</p>
+          <div className="current-weather">
+            <div className="current-temp">
+              <h1>{props.data.main.temp.toFixed()}°C </h1>
+              <p id="current-description">{props.data.weather[0].main}</p>
+              <div id="current-img">
+                <img
+                  src={`http://openweathermap.org/img/w/${props.data.weather[0].icon}.png`}
+                />
+              </div>
+            </div>
           </div>
           <div className="current-bottom">
             <div className="feels">
               <p>feels like</p>
-              <p>{props.data.main.feels_like.toFixed()}°C</p>
+              <p className="bottom-desc">{props.data.main.feels_like.toFixed()}°C</p>
             </div>
             <div className="wind">
               <p>wind speed</p>
-              <p>{props.data.wind.speed} kM/H</p>
+              <p className="bottom-desc">{props.data.wind.speed} kM/H</p>
             </div>
             <div className="humidity">
               <p>humidity</p>
-              <p>{props.data.main.humidity} %</p>
+              <p className="bottom-desc">{props.data.main.humidity} %</p>
             </div>
           </div>
         </>
       ) : null}
-        {/* <button onClick={()=> navigate('/hourly', { replace: true })}>Hourly</button> */}
+      {/* <button onClick={()=> navigate('/hourly', { replace: true })}>Hourly</button> */}
     </div>
   );
 };
