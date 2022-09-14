2133a83e308913641e195582b461ec81b06341aa
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-14 20:34:29 +0200

    edit styles hourly comp

--- src/components/hourly/Hourly.js
+++ src/components/hourly/Hourly.js
@@ -1,9 +1,9 @@
 //import { useHistory } from "react-router-dom";
 import { format } from "date-fns/esm";
+import './Hourly.css'
 
 // provides hourly weather data every 3 hours
 const Hourly = ({ forecastData }) => {
-
   //const history = useHistory();
 
   // const goBack = () => {
@@ -15,7 +15,15 @@ const Hourly = ({ forecastData }) => {
         {forecastData ? (
           <>
             {forecastData.list.splice(0, 10).map((item, index) => (
-              <div className="hourly-data" key={index} style={{ display:'flex', justifyContent: 'space-around' }}>
+              <div
+                className="hourly-data"
+                key={index}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  marginTop: "10px",
+                }}
+              >
                 <p>{format(new Date(item.dt_txt), "MMMM do, yyyy H:mma")}</p>
                 <p key={index} className="hourly-description">
                   {item.weather[0].main}
